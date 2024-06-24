3708e7b39c23bfe0656f23bc9569c4ed41a6a09e
polymahh <otman.elkantaoui@gmail.com>
2024-06-24 19:43:02 +0100

    fix

--- app/(site)/error/page.tsx
+++ app/(site)/error/page.tsx
@@ -1,12 +1,6 @@
 import { Metadata } from "next";
 import Image from "next/image";
 
-export const metadata: Metadata = {
-  title: "Error Page - Solid SaaS Boilerplate",
-  description: "This is Error page for Solid Pro",
-  // other metadata
-};
-
 const ErroPage = () => {
   return (
     <section className="overflow-hidden pb-25 pt-45 lg:pb-32.5 lg:pt-50 xl:pb-37.5 xl:pt-55">
